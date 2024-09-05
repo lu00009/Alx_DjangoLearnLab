9715fe94c702818df9d561e8681441ace82bf5ca
Lelo Mohamed <lelomohammedpg@gmail.com>
2024-09-05 10:48:42 +0300

    adanced dgango

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book
+from django.views.generic import DetailView
+def book_list(request):
+      books = Book.objects.all()  # Fetch all book instances from the database
+      context = {'book_list': books}  # Create a context dictionary with book list
+      return render(request, 'books/book_list.html', context)
+
+
+
+
+
+class BookDetailView(DetailView):
+  model = Book
+  template_name = 'books/book_detail.html'
+
+  def get_context_data(self, **kwargs):
+    """Injects additional context data specific to the book."""
+    context = super().get_context_data(**kwargs)  # Get default context data
+    book = self.get_object()  # Retrieve the current book instance
+    context['average_rating'] = book.get_average_rating() 
