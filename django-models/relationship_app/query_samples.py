4de9e89c9ea5ba8bf8ea2fc87720906a992b91a2
Lelo Mohamed <lelomohammedpg@gmail.com>
2024-09-05 10:12:09 +0300

    advanced model relationshipin dgango

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,11 +1,12 @@
 #To query all books by a specific author
-specific_author = Author.objects.get(name="Author Name")
-books_by_author = Book.objects.filter(author=specific_author)
+author_name = Author.objects.get(name="Author Name")
+books_by_author = Book.objects.filter(author=author_name)
 
 #To list all books in a library
-specific_library = Library.objects.get(name="Library Name")
-books_in_library = Book.objects.filter(library=specific_library)
+library_name = "Library Name"
+library = Book.objects.get(library=library_name)
+books_in_library=library.books.all()
 
 #To retrieve the librarian for a library
-specific_library = Library.objects.get(name="Library Name")
-librarian = Librarian.objects.get(library=specific_library)
+library = Library.objects.get(name=library_name)
+librarian = library.librarian
