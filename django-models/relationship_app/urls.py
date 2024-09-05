889111e6b6f33693ab2ede8299ebbb02988ae65e
Lelo Mohamed <lelomohammedpg@gmail.com>
2024-09-05 14:57:43 +0300

    user authentication

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 from .views import list_books
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>//', views.LibraryDetailView.as_view(), name='LibraryDetailView' )
+    path('library/<int:pk>//', views.LibraryDetailView.as_view(), name='LibraryDetailView' ),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Create your views here.
 from .models import Book
 from django.views.generic import DetailView
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 def book_list(request):
       books = Book.objects.all()  # Fetch all book instances from the database
       context = {'list_books': books}  # Create a context dictionary with book list
       return render(request, 'relationship_app/list_books.html', context)
-
-
-
 class LibraryDetailView(DetailView):
   model = Book
   template_name = 'relationship_app/library_detail.html'
@@ -20,3 +19,15 @@ class LibraryDetailView(DetailView):
     book = self.get_object()  # Retrieve the current book instance
     context['books'] = self.objects.books.all() 
     return context
+  
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log the user in after registration
+            return redirect('home')  # Redirect to the home page after registration
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'register.html', {'form': form})
